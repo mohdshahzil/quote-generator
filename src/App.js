3a90ee53c560402192fa95c8bd82624ac7f46635
mohdshahzil <theshahzil@gmail.com>
2023-09-19 04:52:51 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState("");
+  const [error, setError] = useState("");
+  const url = "https://api.quotable.io/random";
+  const quoteHandler = () => {
+    axios
+      .get(url)
+      .then((res) => {
+        setQuote(res.data.content);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center bg-slate-950 h-screen w-screen text-white">
+      <button
+        onClick={quoteHandler}
+        className="p-2 bg-slate-500 rounded-lg text-white text-2xl "
+      >
+        Generate{" "}
+      </button>
+
+      {error ? (
+        <div className="text-2xl my-4 p-6 text-center">{error}</div>
+      ) : (
+        <div className="text-2xl my-4 p-6 text-center">{quote}</div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
